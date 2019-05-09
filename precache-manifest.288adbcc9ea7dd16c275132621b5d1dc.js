d8569518ea1e2faafef5e8295cc52b677358f5b8
Fedin Dima <dima.groot.04@gmail.com>
2019-05-09 21:10:39 +0300

    Updates

---
+++ precache-manifest.288adbcc9ea7dd16c275132621b5d1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7016a103bc16b2867424",
+    "url": "/vk-quest-app/static/js/runtime~main.8f90014c.js"
+  },
+  {
+    "revision": "0a4eec6e730c42a6e850",
+    "url": "/vk-quest-app/static/js/main.90c3d6db.chunk.js"
+  },
+  {
+    "revision": "60a4effb0167c8442c87",
+    "url": "/vk-quest-app/static/js/2.a03415de.chunk.js"
+  },
+  {
+    "revision": "0a4eec6e730c42a6e850",
+    "url": "/vk-quest-app/static/css/main.34e31c95.chunk.css"
+  },
+  {
+    "revision": "60a4effb0167c8442c87",
+    "url": "/vk-quest-app/static/css/2.8a24a589.chunk.css"
+  },
+  {
+    "revision": "24a82950b1bf72d71b7795da05692928",
+    "url": "/vk-quest-app/index.html"
+  }
+];
